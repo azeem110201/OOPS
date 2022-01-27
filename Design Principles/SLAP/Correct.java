a46861bca36ba8cd6841b12cf1cdccc9db4b5dee
azeem <mohd.abazeem@gmail.com>
2022-01-27 08:22:00 +0530

    added interface support to transaction class

--- Design Principles/SLAP/Correct.java
+++ Design Principles/SLAP/Correct.java
@@ -1,11 +1,11 @@
-// interface Transaction{
-//     public void withdraw(double balance, double amount);
-//     public void deposit(double balance, double amount);
-// }
+interface Transaction{
+    public double withdraw(double balance, double amount);
+    public double deposit(double balance, double amount);
+}
 
 //Above code is optional. If we want to add interface between the NormalTRansaction and the Bank.
 
-class NormalTransaction {
+class NormalTransaction implements Transaction{
     public double withdraw(double balance, double amount) {
         if (balance <= amount) {
             return balance;
@@ -21,8 +21,8 @@ class NormalTransaction {
     }
 }
 
-class CryptoTransaction {
-    public double withdrawCrypto(double cryptoBalance, double amount) {
+class CryptoTransaction implements Transaction{
+    public double withdraw(double cryptoBalance, double amount) {
         if (cryptoBalance <= amount) {
             System.out.println("Not having enough amount");
             return cryptoBalance;
@@ -32,7 +32,7 @@ class CryptoTransaction {
         return cryptoBalance;
     }
 
-    public double depositCrypto(double cryptoBalance, double amount) {
+    public double deposit(double cryptoBalance, double amount) {
         cryptoBalance += amount;
 
         return cryptoBalance;
@@ -51,11 +51,10 @@ class Bank {
         this.balance = amount;
     }
 
-    public void withdraw(double amount) {
+    public void withdraw(NormalTransaction nt, double amount) {
         // added Transaction component which is different then the Bank
-        NormalTransaction trans = new NormalTransaction();
         double balanceCheck = this.balance;
-        this.balance = trans.withdraw(this.balance, amount);
+        this.balance = nt.withdraw(this.balance, amount);
         if (balanceCheck == this.balance) {
             System.out.println("Not having enough amount");
         } else {
@@ -63,11 +62,10 @@ class Bank {
         }
     }
 
-    public void deposit(double amount) {
+    public void deposit(NormalTransaction nt, double amount) {
         // added Transaction component which is different then the Bank
-        NormalTransaction trans = new NormalTransaction();
         double balanceCheck = this.balance;
-        this.balance = trans.deposit(this.balance, amount);
+        this.balance = nt.deposit(this.balance, amount);
         if (balanceCheck == this.balance) {
             System.out.println("Money is not depoisted in the bank");
         } else {
@@ -83,11 +81,10 @@ class Bank {
         this.cryptoBalance = amount;
     }
 
-    public void withdrawCrypto(double amount) {
+    public void withdrawCrypto(CryptoTransaction ct,double amount) {
         // added CrytoTransaction component which is different then the Bank
-        CryptoTransaction trans = new CryptoTransaction();
         double balanceCheck = this.cryptoBalance;
-        this.cryptoBalance = trans.withdrawCrypto(this.cryptoBalance, amount);
+        this.cryptoBalance = ct.withdraw(this.cryptoBalance, amount);
         if (balanceCheck == this.cryptoBalance) {
             System.out.println("Not having enough amount");
         } else {
@@ -95,11 +92,10 @@ class Bank {
         }
     }
 
-    public void depositCrypto(double amount) {
+    public void depositCrypto(CryptoTransaction ct, double amount) {
         // added CrytoTransaction component which is different then the Bank
-        CryptoTransaction trans = new CryptoTransaction();
         double balanceCheck = this.cryptoBalance;
-        this.cryptoBalance = trans.depositCrypto(this.cryptoBalance, amount);
+        this.cryptoBalance = ct.deposit(this.cryptoBalance, amount);
         if (balanceCheck == this.cryptoBalance) {
             System.out.println("Crypto is not depoisted in the bank");
         } else {
@@ -113,16 +109,20 @@ class Correct {
         Bank bank = new Bank();
         bank.setBalance(10000);
         System.out.println(bank.getBalance());
-        bank.withdraw(2000);
-        bank.deposit(3000);
+
+        NormalTransaction nt = new NormalTransaction();
+        bank.withdraw(nt, 2000);
+        bank.deposit(nt, 3000);
         System.out.println(bank.getBalance());
 
 
         bank.setCryptoBalance(10);
         System.out.println(bank.getCryptoBalance());
         bank.getCryptoBalance();
-        bank.withdrawCrypto(2);
-        bank.depositCrypto(3);
+
+        CryptoTransaction ct = new CryptoTransaction();
+        bank.withdrawCrypto(ct, 2);
+        bank.depositCrypto(ct, 3);
         System.out.println(bank.getCryptoBalance());
     }
 }
